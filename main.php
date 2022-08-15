51b2766f493aaad807c43da3a517f82f78d224cf
Maximilian Rudolph <rudolph-max@gmx.net>
2022-08-15 18:43:58 +0200

    main : updates

--- main.php
+++ main.php
@@ -2,31 +2,42 @@
 chdir( $_SERVER['DOCUMENT_ROOT'] );
 require('bootstrap.php');
 
-if (!isset($_GET['p'])) {
-  throw new PageException( 404 );
+function getPageFromString( $string ) {
+  $a = array(
+    'path' => (mb_substr($string, -1) !== '/') ? $string : substr($string, 0, -1) // removing trailing slash (if existing)
+  );
+  $a['pathArray'] = explode('/', $a['path']);
+  $a['name'] = $a['pathArray'][ count($a['pathArray'])-1 ];
+
+  return $a;
 }
 
-$pagePath = $_GET['p'];
-$pagePath = (mb_substr($pagePath, -1) !== '/') ? $pagePath : substr($pagePath, 0, -1); // removing trailing slash (if existing)
-$pagePathArray = explode('/', $pagePath);
-$pageName = $pagePathArray[ count($pagePathArray)-1 ];
+if (!isset($_GET['p'])) {
+   header('HTTP/1.1 404 Not Found');
+   die();
+}
+
+
+$pageObject = getPageFromString( $_GET['p'] );
 
 chdir(locPages);
-foreach( $pagePathArray as $page ) {
-  
+foreach( $pageObject['pathArray'] as $page ) {
+
   if (!file_exists($page) ) {
-    throw new PageException( 404 );
+     header('HTTP/1.1 404 Not Found');
+     die();
   }
 
 }
 chdir(ROOTPATH); // Changing back to the system-root
 
+
 /*
 Setting up the page
 */
 $page = new Page(
-  $pageName,
-  locPages.$pagePath
+  $pageObject['name'],
+  locPages.$pageObject['path']
 );
 if ($page) {
   $page->insert();
