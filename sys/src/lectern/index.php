482a1f13dc2d08e6eea343368a4f8e8ac2d2d79d
Maximilian Rudolph <rudolph-max@gmx.net>
2022-08-18 21:56:33 +0200

    lectern : updates

--- sys/src/lectern/index.php
+++ sys/src/lectern/index.php
@@ -2,7 +2,7 @@
 /**/
 
 error_reporting(E_ALL);
-ini_set("display_errors", 1);
+ini_set('display_errors', 1);
 
 require('../bootstrap.php');
 \mdBacker\sys\includeClasses( __DIR__ );
@@ -19,22 +19,66 @@ $lectern = new \mdBacker\lectern\classes\Lectern();
 <!DOCTYPE html>
 <html>
   <head>
-    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv='Content-Type' content='text/html;charset=UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+
+    <!-- head : external scripts -->
+
+    <!-- head : internal scripts -->
+    <script type='text/javascript'>
+      <?php \mdBacker\sys\requireFilesFromLoader(__DIR__, 'headerScripts'); ?>
+    </script>
 
     <!-- css -->
-    <style type="text/css">
+    <style type='text/css'>
       <?= $lectern->compileScss( '@import "'.__DIR__.'/css/global.scss"' ); ?>
     </style>
   </head>
 
-  <body class="mdBacker lectern">
+  <body class='mdBacker lectern'>
 
-    <header class="header">
-      <p class="h5 bold c-primary">scribunus</p>
-      <p class="h4 light c-dim ">/</p>
-      <p class="h5 light c-grey">lectern</p>
+    <header class='w1 header'>
+      <h1 id='name'>scribunus</h1>
+      <span class='diagonal light'></span>
+      <span id='module'>lectern</span>
     </header>
+
+    <main class='w1'>
+
+      <nav id='main'>
+        <div id='sections' class='accordion'>
+          
+          <div class='section' id='files'>
+            <h2 class='label'>pages</h2>
+            <ul class="content">
+              <li class="file">home</li>
+              <li class="file">about</li>
+              <li class="file">
+                blog
+                <ul>
+                  <li>How I built my own flat-file CMS</li>
+                </ul>
+              </li>
+            </ul>
+
+          </div>
+
+        </div>
+      </nav>
+
+      <div class='content'>
+      </div>
+
+    </main>
+
+
+    <!-- footer : external scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+    <!-- footer : internal scripts -->
+    <script type='text/javascript'>
+      <?php \mdBacker\sys\requireFilesFromLoader(__DIR__, 'footerScripts'); ?>
+    </script>
 
   </body>
 </html>
---
+++ sys/src/lectern/js/main.js
@@ -0,0 +1,6 @@
+$( document ).ready( function() {
+
+  console.log( window.location.search );
+  //$( 'nav#main .accordion#sections .section' )
+
+});
